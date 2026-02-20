1966f840b7d38f2147ee84b6642c547d8f182883
PawelKorsak <p.korsak010@gmail.com>
2026-02-20 11:46:39 +0100

    feature: preparing the json elf read

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use goblin::Object;
+use std::path::Path;
+use tokio::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = std::io::stdin();
+    let stdout = std::io::stdout();
+
+    let json_string = r#"{
+            "executable_path": "path"
+        }"#;
+
+    let json: serde_json::Value = serde_json::from_str(json_string).expect("Invalid JSON format!");
+
+    let path = Path::new(json["executable_path"].as_str().expect("No path provided!"));
+    let buffer = std::fs::read(path).expect("Coudl not read buffer");
+
+    match Object::parse(&buffer) {
+        Ok(object) => match object {
+            Object::Elf(elf) => {
+                println!("elf: {:#?}", &elf);
+            }
+            Object::Unknown(magic) => {
+                println!("unknown magic: {:#x}", magic)
+            }
+            _ => {}
+        },
+        Err(e) => eprintln!("Error parsing object: {}", e),
+    }
 }
