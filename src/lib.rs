fbdd5e9a76bbffd38a70c6d0ccda393918538aad
PawelKorsak <p.korsak010@gmail.com>
2026-03-09 16:01:15 +0100

    feature: connection forwarding

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use core::time;
+
 use zed_extension_api::{
     self as zed, DebugAdapterBinary, DebugConfig, DebugRequest, DebugScenario, DebugTaskDefinition,
     StartDebuggingRequestArguments, StartDebuggingRequestArgumentsRequest, TcpArguments, Worktree,
@@ -25,21 +27,44 @@ impl zed::Extension for ZedCortexDebugRs {
         worktree: &Worktree,
     ) -> Result<DebugAdapterBinary, String> {
         println!("Getting dap...");
+        println!("Adapter Name: {adapter_name}");
+        println!("Config label: {}", config.label);
+        println!("Config adapter: {}", config.adapter);
+        println!("TCP connection details {:?}", config.tcp_connection);
+        println!(
+            "User provided debug adapter path: {:?}",
+            user_provided_debug_adapter_path
+        );
+        println!("Worktree: {:?}", worktree);
+
         let command: Option<String> = Some("echo".to_string());
         let arguments: Vec<String> = vec!["testing zed debugger".to_string()];
 
-        let connecection = TcpArguments {
-            port: 0,
-            host: 0,
-            timeout: None,
+        let port = match config.tcp_connection {
+            Some(conn) => conn.port.ok_or_else(|| "port not provided")?,
+            None => return Err("tcp_connection not provided".to_string()),
         };
+        let host = match config.tcp_connection {
+            Some(conn) => conn.host.ok_or_else(|| "host not provided")?,
+            None => return Err("tcp_connection not provided".to_string()),
+        };
+        let timeout = match config.tcp_connection {
+            Some(conn) => conn.timeout,
+            None => None,
+        };
+
+        let connection = Some(TcpArguments {
+            port,
+            host,
+            timeout,
+        });
 
         Ok(DebugAdapterBinary {
             command,
             arguments,
             envs: vec![],
             cwd: None,
-            connection: None,
+            connection,
             request_args: StartDebuggingRequestArguments {
                 // We just pass along the configuration
                 configuration: config.config,
